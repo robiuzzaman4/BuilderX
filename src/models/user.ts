000df118600d9107a49362ba3a1d3bc977440c5e
ruhan <robiuzzaman4@gmail.com>
2025-11-01 14:19:24 +0600

    feat: define user types and make user model

---
+++ src/models/user.ts
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// === hash password before save into db ===
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+export const User = model("User", userSchema);
