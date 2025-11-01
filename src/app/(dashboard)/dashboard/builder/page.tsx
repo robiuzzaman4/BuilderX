47d2776c5ad12055e82c77ec21e8012587853a7c
ruhan <robiuzzaman4@gmail.com>
2025-11-01 22:05:27 +0600

    feat: dashboard and builder layout setup done

---
+++ src/app/(dashboard)/dashboard/builder/page.tsx
@@ -0,0 +1,10 @@
+const BuilderPage = () => {
+  return (
+    <div className="bg-muted h-full w-full grid lg:grid-cols-2 gap-4 px-4 py-4">
+      <div className="h-full p-6 rounded-lg bg-background border">Hello</div>
+      <div className="h-full p-6 rounded-lg bg-background border">Hello</div>
+    </div>
+  );
+};
+
+export default BuilderPage;
